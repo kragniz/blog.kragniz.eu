ef842b086779ed79c5e8b58ee98c21fffc64bc37
Louis Taylor <kragniz@gmail.com>
2015-02-09 23:34:03 +0000

    Base setup

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Louis Taylor'
+SITENAME = u'blog.kragniz.eu'
+SITEURL = ''
+
+PATH = 'content'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME='pelican-svbhack'
+
+STATIC_PATHS = ['images']
+USER_LOGO_URL = "images/lpt_gliph.svg"
+TAGLINE='くりいゃん'
+
+# Blogroll
+LINKS = (('Github', 'https://github.com/kragniz'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/kragniz'),)
+
+#DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
